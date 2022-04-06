61d75607c48511cb7bc75b84f990863198054914
Jonathan Chosson <jonathan.chosson@gmail.com>
2022-04-06 15:57:25 +0200

    [CSS/JS] Ajout des conférences, modification mode mobile :iphone:

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -13,6 +13,7 @@ const TeamCard = ({teamUrl}) => {
                 if(response.data.venue.images.length !== 0){
                     setTeamStadeImage = response.data.venue.images[0].href
                 }
+                let groups = await axios.get(response.data.groups.$ref)
                 let team = {
                     teamName: response.data.shortDisplayName,
                     teamLogo: response.data.logos[0].href,
@@ -20,7 +21,8 @@ const TeamCard = ({teamUrl}) => {
                     teamCompleteName: response.data.displayName,
                     teamCity: response.data.venue.address.city,
                     teamStadeName: response.data.venue.fullName,
-                    teamStadeImage: setTeamStadeImage
+                    teamStadeImage: setTeamStadeImage,
+                    teamGroups: groups.data.name
                 }
                 setTeamInfo(team)
         }
@@ -58,11 +60,11 @@ const TeamCard = ({teamUrl}) => {
                 <img className='section__img' src={teamInfo.teamLogo} alt={"Logo de " + teamInfo.TeamName} />
                 <p className='section__p'>{teamInfo.teamName}</p>
             </section>
-            <section className='section--info invisible' >
+            <section className='section--info invisible' onClick={handleClickDos}>
                 {teamInfo.teamStadeImage !== "" ? <img className='section__img' src={teamInfo.teamStadeImage} alt={"Stade de " + teamInfo.teamName} /> : ""}
-                <div className='section--info__div' onClick={handleClickDos}>
+                <div className='section--info__div' >
                     <p className='section--info__p bold'>{teamInfo.teamCompleteName}</p>
-                    <p className='section--info__p'>{teamInfo.teamAbbreviation}</p>
+                    <p className='section--info__p'>{teamInfo.teamGroups} - {teamInfo.teamAbbreviation}</p>
                     <p className='section--info__p'>Stade : {teamInfo.teamStadeName}</p>
                     <p className='section--info__p'>Ville : {teamInfo.teamCity}</p>
                 </div>
